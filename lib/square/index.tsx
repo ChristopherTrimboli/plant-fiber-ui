7b01fcfde216264ff0cf1daf13db0b3614265a84
Chris <chris.eddy.dev@gmail.com>
2020-06-11 00:09:34 -0700

    major work, remove demo code, .babelrc, build changes

--- lib/square/index.tsx
+++ lib/square/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Square = (props: any) => {
     return (
         <>
-            <p>Square</p>
+            <p>LOL</p>
         </>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: './lib/index.tsx',
+  entry: './lib/index.js',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: 'plant.fiber.ui.js',
